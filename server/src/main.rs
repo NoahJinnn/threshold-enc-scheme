fc865d774479967ce5b2d8a8ea79bb3372f2efc2
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-06-21 13:17:11 +0700

    feat(server/main.rs): Create /init route to initialize keypair and repair for commit route

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,22 +1,31 @@
 pub mod dkg;
 use axum::{
-    error_handling::HandleErrorLayer,
-    extract::{Path, Query, State},
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, patch},
-    Json, Router,
+    error_handling::HandleErrorLayer, extract::State, http::StatusCode, response::IntoResponse,
+    routing::post, Json, Router,
 };
+use axum_macros::debug_handler;
+use dkg::{Ack, NodeIdT, Part, PubKeyMap, SyncKeyGen};
 use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashMap,
+    borrow::Borrow,
+    collections::{BTreeMap, HashMap},
     net::SocketAddr,
     sync::{Arc, RwLock},
     time::Duration,
 };
+use threshold_crypto::SecretKey;
 use tower::{BoxError, ServiceBuilder};
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+type Db = Arc<RwLock<HashMap<usize, Session>>>;
+struct Session {
+    sk: SecretKey,
+    node: SyncKeyGen<usize>,
+    parts: Vec<Part>,
+    acks: Vec<Ack>,
+    success: bool,
+}
 
 #[tokio::main]
 async fn main() {
@@ -28,12 +37,14 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    // let db = Db::default();
+    let db = Db::default();
 
     // Compose the routes
     let app = Router::new()
-        // .route("/todos", get(todos_index).post(todos_create))
-        // .route("/todos/:id", patch(todos_update).delete(todos_delete))
+        .route("/init_dkg", post(init_dkg))
+        .route("/commit", post(commit))
+        // .route("/commit_ack", post(commit_ack))
+        // .route("/finalize_dkg", post(finalize_dkg))
         // Add middleware to all routes
         .layer(
             ServiceBuilder::new()
@@ -50,8 +61,8 @@ async fn main() {
                 .timeout(Duration::from_secs(10))
                 .layer(TraceLayer::new_for_http())
                 .into_inner(),
-        );
-        // .with_state(db);
+        )
+        .with_state(db);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -61,112 +72,67 @@ async fn main() {
         .unwrap();
 }
 
-// test
-#[cfg(test)]
-mod test {
-    use std::collections::BTreeMap;
-    use std::sync::Arc;
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct InitDkgReq {
+    p1_pk: threshold_crypto::PublicKey,
+}
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct InitDkgResp {
+    p0_pk: threshold_crypto::PublicKey,
+    p0_part: Part,
+}
+#[debug_handler]
+async fn init_dkg(State(db): State<Db>, Json(req_body): Json<InitDkgReq>) -> impl IntoResponse {
+    let mut rng = rand::rngs::OsRng::new().expect("Could not open OS random number generator.");
+    let threshold = 0;
+    let sk: SecretKey = rand::random();
+    let p0_pk = sk.public_key();
+    let mut map = BTreeMap::new();
+    map.insert(0, p0_pk.clone());
+    map.insert(1, req_body.p1_pk.clone());
+    let pub_keys: PubKeyMap<usize, threshold_crypto::PublicKey> = Arc::new(map);
+    let (sync_key_gen, opt_part) =
+        SyncKeyGen::new(0, sk.clone(), pub_keys.clone(), threshold, &mut rng)
+            .unwrap_or_else(|_| panic!("Failed to create `SyncKeyGen` instance for node #{}", 0));
+    let parts = vec![opt_part.unwrap().clone()];
+    let resp = InitDkgResp {
+        p0_pk: p0_pk.clone(),
+        p0_part: parts[0].clone(),
+    };
+    let acks = vec![];
+    let session = Session {
+        sk,
+        node: sync_key_gen,
+        parts,
+        acks,
+        success: false,
+    };
+    db.write().unwrap().insert(0, session);
 
-    use super::dkg::{to_pub_keys, AckOutcome, PartOutcome, PubKeyMap, SyncKeyGen};
-    use threshold_crypto::{SecretKey, SignatureShare};
 
-    #[test]
-    fn test_all() {
-        // Use the OS random number generator for any randomness:
-        let mut rng = rand::rngs::OsRng::new().expect("Could not open OS random number generator.");
+    Json(resp)
+}
 
-        // Two out of four shares will suffice to sign or encrypt something.
-        let (threshold, node_num) = (0, 2);
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct CommitReq {
+    p1_part: Part,
+    p1_ack: Ack,
+}
 
-        // Generate individual key pairs for encryption. These are not suitable for threshold schemes.
-        let sec_keys: Vec<SecretKey> = (0..node_num).map(|_| rand::random()).collect();
-        let pub_keys = to_pub_keys(sec_keys.iter().enumerate());
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct CommitResp {
+    p0_ack: Ack,
+}
 
-        // Create the `SyncKeyGen` instances. The constructor also outputs the part that needs to
-        // be sent to all other participants, so we save the parts together with their sender ID.
-        let mut nodes = BTreeMap::new();
-        let mut parts = Vec::new();
-        for (id, sk) in sec_keys.into_iter().enumerate() {
-            let (sync_key_gen, opt_part) =
-                SyncKeyGen::new(id, sk, pub_keys.clone(), threshold, &mut rng).unwrap_or_else(
-                    |_| panic!("Failed to create `SyncKeyGen` instance for node #{}", id),
-                );
-            nodes.insert(id, sync_key_gen);
-            parts.push((id, opt_part.unwrap())); // Would be `None` for observer nodes.
-        }
-
-        println!("nodes: {:?}", nodes);
-        println!("parts: {:?}", parts);
-
-        // All nodes now handle the parts and send the resulting `Ack` messages.
-        let mut acks = Vec::new();
-        for (sender_id, part) in parts {
-            for (&id, node) in &mut nodes {
-                match node
-                    .handle_part(&sender_id, part.clone(), &mut rng)
-                    .expect("Failed to handle Part")
-                {
-                    PartOutcome::Valid(Some(ack)) => acks.push((id, ack)),
-                    PartOutcome::Invalid(fault) => panic!("Invalid Part: {:?}", fault),
-                    PartOutcome::Valid(None) => {
-                        panic!("We are not an observer, so we should send Ack.")
-                    }
-                }
-            }
-        }
-
-        // Finally, we handle all the `Ack`s.
-        for (sender_id, ack) in acks {
-            for node in nodes.values_mut() {
-                match node
-                    .handle_ack(&sender_id, ack.clone())
-                    .expect("Failed to handle Ack")
-                {
-                    AckOutcome::Valid => (),
-                    AckOutcome::Invalid(fault) => panic!("Invalid Ack: {:?}", fault),
-                }
-            }
-        }
-
-        // We have all the information and can generate the key sets.
-        // Generate the public key set; which is identical for all nodes.
-        let pub_key_set = nodes[&0]
-            .generate()
-            .expect("Failed to create `PublicKeySet` from node #0")
-            .0;
-        let mut secret_key_shares = BTreeMap::new();
-        for (&id, node) in &mut nodes {
-            assert!(node.is_ready());
-            let (pks, opt_sks) = node.generate().unwrap_or_else(|_| {
-                panic!(
-                    "Failed to create `PublicKeySet` and `SecretKeyShare` for node #{}",
-                    id
-                )
-            });
-            assert_eq!(pks, pub_key_set); // All nodes now know the public keys and public key shares.
-            let sks = opt_sks.expect("Not an observer node: We receive a secret key share.");
-            secret_key_shares.insert(id, sks);
-        }
-
-        println!("secret_key_shares {:?}", secret_key_shares);
-
-        // Two out of four nodes can now sign a message. Each share can be verified individually.
-        let msg = "Nodes 0 and 1 does not agree with this.";
-        let mut sig_shares: BTreeMap<usize, SignatureShare> = BTreeMap::new();
-        for (&id, sks) in &secret_key_shares {
-            // if id != 0 && id != 1 {
-                let sig_share = sks.sign(msg);
-                let pks = pub_key_set.public_key_share(id);
-                assert!(pks.verify(&sig_share, msg));
-                sig_shares.insert(id, sig_share);
-            // }
-        }
-
-        // Two signatures are over the threshold. They are enough to produce a signature that matches
-        // the public master key.
-        let sig = pub_key_set
-            .combine_signatures(&sig_shares)
-            .expect("The shares can be combined.");
-        assert!(pub_key_set.public_key().verify(&sig, msg));
-    }
+async fn commit(State(db): State<Db>, Json(req_body): Json<CommitReq>) -> impl IntoResponse {
+    let mut parts = db.read().unwrap().get(&0).unwrap().parts.clone();
+    parts.insert(1, req_body.p1_part.clone());
+    let mut acks = vec![];
+    acks.insert(1, req_body.p1_ack.clone());
+    // db.write().unwrap().insert(todo.id, todo.clone());
+    let resp = CommitResp {
+        p0_ack: acks[0].clone(),
+    };
+    
+    Json(resp)
 }
